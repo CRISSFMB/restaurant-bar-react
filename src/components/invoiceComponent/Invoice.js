921ed0c7aa03a97846bb1a2732116aa8c9eafdd9
cristian mosquera <cmosqbone1@gmail.com>
2022-07-11 12:00:27 +0200

    add styles to the payment section

--- src/components/invoiceComponent/Invoice.js
+++ src/components/invoiceComponent/Invoice.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import { Check } from '../icons/Check';
+import imgPayment from '../../assets/svgs/payment-icon.svg';
+
 import './invoice.css';
 
 export const Invoice = () => {
   return (
-    <div className="Invoice">
-      {/* bill-wrapper */}
-      <div className="bill-wrapper">
-        <div className="header-invoice">
-          <Check />
-          <p className="header-invoice__paragraph">Payment Successfull</p>
-        </div>
-        <div className="wrapper-info-bill">
-          <div>
-            <p>Name</p>
-            <p>Address</p>
-            <p>Email</p>
-            <p>TelefonNumer</p>
-            <p>Method</p>
-            <p>amount</p>
-            <p>date Delivery</p>
-            <p>time Delive</p>
-          </div>
-          {/* ---- */}
-          <div>
-            <p>Cristian</p>
-            <p>Hohohnsweg 3</p>
-            <p>cmosqbone1@gmail.com</p>
-            <p>872309ß1293</p>
-            <p>cash</p>
-            <p>$234</p>
-            <p>12/ 05 / 2022</p>
-            <p>11 am</p>
+    <>
+      <div className="Invoice">
+        <div className="bill-wrapper">
+          {/* <p className="Invoice-title">Congratulations!</p> */}
+          <div className="flex-bill">
+            <div className="img-payment">
+              <img src={imgPayment} />
+            </div>
+
+            <div className="wrapper-info-bill">
+              <div>
+                <p>Name</p>
+                <p>Address</p>
+                <p>Email</p>
+                <p>TelefonNumer</p>
+                <p>Method</p>
+                <p>amount</p>
+                <p>date Delivery</p>
+                <p>time Delive</p>
+              </div>
+              {/* ---- */}
+              <div>
+                <p>Cristian</p>
+                <p>Hohohnsweg 3</p>
+                <p>cmosqbone1@gmail.com</p>
+                <p>872309ß1293</p>
+                <p>cash</p>
+                <p>$234</p>
+                <p>12/ 05 / 2022</p>
+                <p>11 am</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
